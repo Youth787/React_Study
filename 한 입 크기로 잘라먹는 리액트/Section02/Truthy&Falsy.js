c0fa17313e5dc565d66248867a34f0bf0a7438d0
yeonmi <ar77gt@daum.net>
2024-02-20 11:21:55 +0900

    add : section02-truethy&falsy

---
+++ 한 입 크기로 잘라먹는 리액트/Section02/Truthy&Falsy.js
@@ -0,0 +1,27 @@
+/* 
+// true로 인식 TRUTHY (참이 아닌데 참으로 판단되는 값)
+let a = "string"; 
+let a = 42; 
+let a = "0";
+let a = "false";
+let a = Infinity;
+let a = []; 
+let a = {}; 
+
+// false로 인식 falsy (거짓이 아닌데 거짓으로 판단되는 값)
+let a = null;
+let a = undefined;
+let a = -0;
+let a = 0;
+let a = NaN;
+let a = ""; 
+*/
+
+if(a){
+    console.log("TRUE");
+}else{
+    console.log("FALSE");
+}
+
+
+

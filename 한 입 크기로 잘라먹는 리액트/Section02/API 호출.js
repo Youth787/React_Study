58ddc1473e8ba2b4086ecc4b55eff5a963db6fda
정연미 <ar77gt@daum.net>
2024-02-20 20:14:22 +0900

    add : section02

---
+++ 한 입 크기로 잘라먹는 리액트/Section02/API 호출.js
@@ -0,0 +1,10 @@
+// json placeholder 
+
+async function getData() {
+    let rawResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let jsonResponse = await rawResponse.json();
+    console.log(jsonResponse);
+  }
+  
+  getData();
+  

2b5ea845a227919d2abd3bca4305c84923eef0f9
yeonmi <ar77gt@daum.net>
2024-02-23 14:01:05 +0900

    simplediary

--- 한 입 크기로 잘라먹는 리액트/SimpleDiary/src/App.js
+++ 한 입 크기로 잘라먹는 리액트/SimpleDiary/src/App.js
@@ -1,13 +1,39 @@
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이도현",
+    content: "하이 1",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "가보자",
+    content: "하이 2",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "이도훈",
+    content: "하이 3",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <DiaryEditor/>
+      <DiaryEditor />
+      <DiaryList diaryList = {dummyList} />
     </div>
   );
 }
 
 export default App;
- 
